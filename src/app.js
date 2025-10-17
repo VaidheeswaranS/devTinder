515558c52c586ca931c252a5732215860fa5b129
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-10-17 18:35:36 +0530

    feat: installed mongoose and defined the schema and models and wrote API for signup adding some dummy users to DB

--- src/app.js
+++ src/app.js
@@ -1,54 +1,34 @@
 ﻿const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
+const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
 const app = express();
 
-/* checking the authorization here, since we use "use" we are checking for all HTTP methods
-all "/admin/*" requests will be resolved only when the authorization is success */
-app.use("/admin", adminAuth);
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "virat.kohli@gmail.com",
+    password: "virat@12345",
+  });
 
-app.post("/user/login", (req, res) => {
-  // logic to handle the login request
-  res.send("User logged in successfully");
+  try {
+    await user.save();
+    res.send("User Added successfully");
+  } catch (err) {
+    res.status(500).send("Error saving the user" + err.message);
+  }
 });
 
-app.post("/user/signUp", (req, res) => {
-  // logic to handle the signup request
-  res.send("User is registered successfully");
-});
-
-/* Authorization needed here to get the information about the user */
-app.get("/user/getData", userAuth, (req, res) => {
-  // logic to get the user data
-  res.send("User data sent");
-});
-
-app.get("/user/login", (req, res) => {
-  res.send("User data is sent");
-});
-
-/* this will be executed only when the authorization is success */
-app.get("/admin/getAllData", (req, res) => {
-  // Logic to get all the data
-  res.send("All data is sent");
-});
-
-/* this will be executed only when the authorization is success */
-app.get("/admin/deleteUser", (req, res) => {
-  // Logic to delete the user
-  res.send("Deleted the user");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from server !!");
-});
-
-app.use("/", (req, res) => {
-  res.send("Namaste Vaidheeswaran");
-});
-
-app.listen(7777);
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(7777, () => {
+      console.log("Server is listening on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed");
+    console.error(err);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+﻿const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://namastedev:2eB33FgRc1OT2zfI@nastenode.0zwa9ht.mongodb.net/devTinder"
+  );
+};
+
+module.exports = {
+  connectDB,
+};
+
+/**
+ *
+ */
