c4b12098520ddf9ce37278e8d55023d72006ab33
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-10-17 12:00:11 +0530

    feat: installed express and created a simple server and serving requests on dummy paths

---
+++ src/app.js
@@ -0,0 +1,32 @@
+﻿const express = require("express");
+
+const app = express();
+
+/* NOT WORKING, when checked in AI, it is asking to use app.get()
+app.use("/", (req, res) => {
+  res.send("Namaste Vaidheeswaran");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello Hello");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from server !!");
+});
+*/
+
+/* This is WORKING */
+app.get("/", (req, res) => {
+  res.send("Namaste Vaidheeswaran");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello Hello Hello");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello from server !!");
+});
+
+app.listen(7777);
