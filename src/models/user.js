71e81907fddac85f863349c6d7d4ea525c51b7a3
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-10-18 16:33:05 +0530

    feat: schema validations, timestamps are added to make the data sanitized

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,63 @@
 ﻿const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  emailId: String,
-  password: String,
-  age: Number,
-  gender: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 1,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minLength: 1,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+      type: String,
+      default: "This is the default bio about the user",
+    },
+    skills: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
