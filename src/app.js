bf30928a3d16e29bde36cf6a1e6472ae033a2f38
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-10-18 11:36:34 +0530

    created the feed and user(get, delete, patch) API's

--- src/app.js
+++ src/app.js
@@ -5,19 +5,78 @@ const { User } = require("./models/user");
 
 const app = express();
 
+// this is the middleware provided by Express which will read the request body and convert it into JS object
+app.use(express.json());
+
+// signup API
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "virat.kohli@gmail.com",
-    password: "virat@12345",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User Added successfully");
   } catch (err) {
     res.status(500).send("Error saving the user" + err.message);
+  }
+});
+
+// user API - this is to get only one user from the database based on emailId
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+// feed API - this is to get all the users in the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+// user API(for DELETE) - this is to delete the user from database based on userId
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await User.findByIdAndDelete({ _id: userId });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User is deleted successfully");
+    }
+  } catch (err) {
+    res.send(400).send("Something went wrong" + err.message);
+  }
+});
+
+// user API (for PATCH) - this is to update the user in the database based on userId
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const data = req.body;
+    const user = await User.findByIdAndUpdate(userId, data);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User is updated successfully");
+    }
+  } catch (err) {
+    res.send(400).send("Something went wrong" + err.message);
   }
 });
 
